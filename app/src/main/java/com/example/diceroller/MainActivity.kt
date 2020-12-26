2cf9e4ef85f0ae551752eff52ca10828bdbad862
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-12-26 16:34:21 +0100

    Basic Dice Roll with images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,20 +14,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.button)
 
+
         rollButton.setOnClickListener() {
             rollDice()
         }
 
+        rollDice()
 
     }
 
     private fun rollDice() {
         val dice = Dice(6)
-        val secondDice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = dice.roll().toString()
-        val secondTextView: TextView = findViewById(R.id.textView3)
-        secondTextView.text = secondDice.roll().toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
+        val roll = dice.roll()
+        when (roll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        diceImage.contentDescription = roll.toString()
     }
 }
 
